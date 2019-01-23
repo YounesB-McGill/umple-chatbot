7968ddb420f2dbd5b14c0336a93eb8aa761dfe28
Murtaza Rizvi <murtazarizvi@Murtazas-MacBook-Pro.local>
2019-01-23 00:08:23 -0500

    simple chat app

---
+++ index.js
@@ -0,0 +1,83 @@
+// Example 1: sets up service wrapper, sends initial message, and 
+// receives response.
+
+var prompt = require('prompt-sync')();
+var AssistantV2 = require('watson-developer-cloud/assistant/v2');
+
+// Set up Assistant service wrapper.
+var service = new AssistantV2({
+  iam_apikey: 'TmWUrfNCwjq4dWbL2YiZv996b3Mlu51tNyuK0PQikqLW', // replace with API key
+  version: '2018-09-20'
+});
+
+var assistantId = '6ef64232-95b9-45c2-a845-6d939b8ab1dc'; // replace with assistant ID
+var sessionId;
+
+// Create session.
+service.createSession({
+  assistant_id: assistantId
+}, function(err, result) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+  sessionId = result.session_id;
+  sendMessage(''); // start conversation with empty message
+});
+
+// Send message to assistant.
+function sendMessage(messageText) {
+  service.message({
+    assistant_id: assistantId,
+    session_id: sessionId,
+    input: {
+      message_type: 'text',
+      text: messageText
+    }
+  }, processResponse);
+}
+
+// Process the response.
+function processResponse(err, response) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+
+  var endConversation = false;
+
+  // Check for client actions requested by the assistant.
+  if (response.output.actions) {
+    if (response.output.actions[0].type === 'client'){
+      if (response.output.actions[0].name === 'display_time') {
+        // User asked what time it is, so we output the local system time.
+        console.log('The current time is ' + new Date().toLocaleTimeString() + '.');
+      } else if (response.output.actions[0].name === 'end_conversation') {
+        // User said goodbye, so we're done.
+        console.log(response.output.generic[0].text);
+        endConversation = true;
+      }
+    }
+  } else {
+    // Display the output from assistant, if any. Assumes a single text response.
+    if (response.output.generic.length != 0) {
+      console.log(response.output.generic[0].text);
+    }
+  }
+
+  // If we're not done, prompt for the next round of input.
+  if (!endConversation) {
+    var newMessageFromUser = prompt('>> ');
+    sendMessage(newMessageFromUser);
+  } else {
+    service.deleteSession({
+      assistant_id: assistantId,
+      session_id: sessionId
+    }, function(err, result) {
+      if (err) {
+        console.error(err); // something went wrong
+      }
+    });
+    return;
+  }
+}
